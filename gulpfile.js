23b5b0ecabc0894088411dbfea240217d66054b2
lorenz068 <laurent.bonnot@gmail.com>
2015-10-21 14:38:18 -0400

    Refactor and fix coding style

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
-// Add dependencies
-var gulp = require("gulp");
+// Gulp dependencies
 var path = require("path");
+var gulp = require("gulp");
 var util = require("gulp-util");
 var concat = require("gulp-concat");
 var sourceMaps = require("gulp-sourcemaps");
 var minifyCSS = require("gulp-minify-css");
 var postcss = require("gulp-postcss");
 var plumber = require("gulp-plumber");
+
+// PostCSS dependencies
 var reporter = require("postcss-reporter")
 var stylelint = require("stylelint");
 var doiuse = require("doiuse");
@@ -15,75 +17,79 @@ var precss = require("precss")
 // Check environnement
 var isDev = !!util.env.dev
 
-// Config of the App
+// App Config 
 var config = {
-    ressource : "Content",
-    destination : "./Build",
+    sourcePath: "Content",
+    sourceType: {
+        css: {
+            app: "/Css/App/**/*.css",
+            external: "/Css/External/**/*.css"
+        }
+    },
+    destinationPath: "./Build",
     supportedBrowsers: [
         "last 2 version",
-        "> 5% in ca", 
+        "> 5% in ca",
         "ie >= 8"
     ]
 };
 
 /* 
- * Tasks : Defaults
+ * Tasks: Defaults
  * By default build CSS & watch modifications 
  */
-gulp.task("default", ["cssApp","cssExternal","watch"]);
+gulp.task("default", ["cssExternal", "cssApp", "watch"]);
 
 /* 
- * Tasks : watch modifications
+ * Tasks: watch modifications
  */
-gulp.task("watch", function() {
-    gulp.watch([config.ressource + "/Css/App/**/*.css" ], ["cssApp"]);
+gulp.task("watch", function () {
+    gulp.watch([config.sourcePath + config.sourceType.css.app], ["cssApp"]);
 });
 
 /* 
- * Tasks : Build CSS of the App
+ * Tasks: Build External CSS
  */
-gulp.task("cssApp", function() {
-  gulp.src([config.ressource + "/Css/App/**/*.css" ], { cwd: process.cwd() })
-  .pipe(plumber())
-  .pipe(postcss([
+gulp.task("cssExternal", function () {
+    gulp.src([config.sourcePath + config.sourceType.css.external], { cwd: process.cwd() })
+        .pipe(plumber())
+        .pipe(sourceMaps.init())
+        .pipe(concat("project.external.css"))
+        .pipe(minifyCSS())
+  
+        // In dev write the sourceMaps
+        .pipe(isDev ? sourceMaps.write() : util.noop())
+        .pipe(gulp.dest(config.destinationPath))
+});
+
+/* 
+ * Tasks: Build CSS of the App
+ */
+gulp.task("cssApp", function () {
+    gulp.src([config.sourcePath + config.sourceType.css.app], { cwd: process.cwd() })
+        .pipe(plumber())
+        .pipe(postcss([
         
-        // Check if rules are supported by browser
-        doiuse({
-            browsers: config.supportedBrowsers,
-            ignore: ["css-mediaqueries"]
-        }),
+            // Check if rules are supported by browser
+            doiuse({
+                browsers: config.supportedBrowsers,
+                ignore: ["css-mediaqueries"]
+            }),
 
-        // Check coding style        
-        stylelint(
-            {"extends": "./.stylelintrc"}
-        ),
+            // Check coding style        
+            stylelint({ "extends": "./.stylelintrc" }),
 
-        // Transform CSS like SASS        
-        precss(),
+            // Transform in like SASS way        
+            precss(),
 
-        // Flag warning        
-        reporter({ clearMessages: true })
-    ]))
-  .pipe(sourceMaps.init())
-  .pipe(concat("project.app.css"))
+            // Flag warning if error    
+            reporter({ clearMessages: true })
+        ]))
+        .pipe(sourceMaps.init())
+        .pipe(concat("project.app.css"))
+        .pipe(minifyCSS())
   
-  // In dev write the sourceMaps
-  .pipe(minifyCSS())
-  .pipe(isDev ? sourceMaps.write() : util.noop())
-  .pipe(gulp.dest(config.destination))
-});
-
-/* 
- * Tasks : CSS External
- */
-gulp.task("cssExternal", function() {
-  gulp.src([config.ressource + "/Css/External/**/*.css" ], { cwd: process.cwd() })
-  .pipe(plumber())
-  .pipe(sourceMaps.init())
-  .pipe(concat("project.external.css"))
-  
-  // In dev write the sourceMaps
-  .pipe(minifyCSS())
-  .pipe(isDev ? sourceMaps.write() : util.noop())
-  .pipe(gulp.dest(config.destination))
+        // In dev write the sourceMaps
+        .pipe(isDev ? sourceMaps.write() : util.noop())
+        .pipe(gulp.dest(config.destinationPath))
 });
